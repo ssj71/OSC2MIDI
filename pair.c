96bf689b70a2076b7cc9b8c67b367a7f02d78ce3
ssj71 <ssjackson71@gmail.com>
2014-11-10 23:16:52 -0700

    working out trymatch, going to add flags

--- pair.c
+++ pair.c
@@ -11,17 +11,21 @@ typedef struct _pair
 
     //osc data
     char**   path;
+    int* perc;//point in path string with printf format %
     lo_arg** argv;//arguments
+    lo_arg** argv_in_path;
     int argc;
     int argc_in_path;
     char* types;
+    float* scale;
+    float* offset;
     
     //conversion factors
     float** coeff;
     float** offset;
 
     //midi data
-    uint8_t midi[3];
+    uint8_t midi[5];
     uint8_t opcode;
     uint8_t channel;
     uint8_t data1;
@@ -67,6 +71,13 @@ int alloc_pair(pair* p, char* config)
     prev = path;
     j = 0;
     char var[100];
+    //figure out how many chunks it will be broken up into
+    tmp = path;
+    i = 1;
+    while(tmp = strchr(tmp,'<'))i++;
+    p->path = (char**)malloc(sizeof(char*)*i);
+    p->perc = (int*)malloc(sizeof(int)*i);
+    //now break it up into chunks
     while(tmp = strchr(prev,'<'))
     {
         //get size of this part of path and allocate a string
@@ -84,6 +95,7 @@ int alloc_pair(pair* p, char* config)
             {
                 free(p->path[p->argc_in_path--]);
             }
+            free(p->path);
             free(p)
             return -1;
         }
@@ -96,6 +108,7 @@ int alloc_pair(pair* p, char* config)
             p->path[p->argc_in_path][j++] = prev[i];
         }
         p->path[p->argc_in_path][j] = 0;//null terminate to be safe
+        p->perc[p->argc_in_path] = j;//mark where the format percent sign is
         strcat(p->path[p->argc_in_path++],"%i")
         prev++ = strchr(prev,'>');
     }
@@ -124,13 +137,54 @@ int alloc_pair(pair* p, char* config)
 
 int free_pair(pair* p)
 {
-    while(p->argc_in_path)
+    while(p->argc_in_path>=0)
         free(p->path[p->argc_in_path--]);
-    free(p->path[p->argc_in_path]);
+    free(p->path);
+    free(p->perc);
+    free(p->arg_in_path);
+    free(p->argv);
     free(p);
 }
 
-int try_match_osc(pair* p, char* oscpath, lo_arg** argv, int argc);
+//returns 1 if match is successful
+int try_match_osc(pair* p, char* path, char* types, lo_arg** argv, int argc)
+{
+    //check the easy things first
+    if(argc < p->argc)
+    {
+       return 0;
+    }
+    if(strncmp(types,p->types,strlen(p->types)))
+    {
+       return 0;
+    }
+
+
+    //now start trying to get the data
+    int i,v;
+    char *tmp;
+    for(i=0;i<p->argc_in_path;i--)
+    {
+  
+        //does it match?
+        p->path[p->perc[i]] = 0;
+        tmp = strstr(path,p->path[i]);  
+        p->path[p->perc[i]] = '%';
+        if(!tmp)
+        {
+            return 0;
+        }
+        //get the argument
+        if(!sscanf(tmp,p->path[i],&v))
+        {
+            return 0;
+        }
+        
+        midi[p->map_in_path[i]] = (uint8_t)(p->scale_in_path[i]*v + p->offset_in_path);
+        //add arg flag if arg then get index
+        
+    }
+}
 int set_channel(pair* p, uint8_t channel);
 int try_match_midi(pair*, uint8_t msg[]);
 uint8_t* get_midi(pair* p);
