dad31c98a32bc097ef6b8e75312072dd618b7fb2
ssj71 <ssjackson71@gmail.com>
2014-12-12 13:24:45 -0700

    added converter.h

---
+++ converter.h
@@ -0,0 +1,22 @@
+//spencer jackson
+//converter.h
+
+//this is a data struct used to pass data from the OSC server thread to the midi thread
+#ifndef CONVERTER_H
+#define CONVERTER_H
+#include<stdint.h>
+#include"pair.h"
+
+typedef struct _CONVERTER
+{
+    uint8_t glob_chan;
+    uint8_t verbose;
+    uint8_t mon_mode;
+    uint8_t multi_match;
+
+    uint16_t npairs;
+    PAIRHANDLE* p;
+
+    //jack ringbuffer probably
+} CONVERTER;
+#endif
